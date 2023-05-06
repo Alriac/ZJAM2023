5badc6e10fa18be86d1bd54657806af6d030465c
d21927790d <dedepankeke@gmail.com>
2023-05-06 12:44:32 +0200

    Mini escenario

---
+++ Assets/Scripts/Enviroment/ObjectInteraction.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectInteraction : MonoBehaviour
+{
+    SpriteRenderer sr;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        sr.color = Color.blue;
+        Debug.Log("hola");        
+    }
+
+    private void OnTriggerExit2D(Collider2D other) {
+        sr.color = Color.white;
+        //Console.Log("hola");        
+    }
+}
