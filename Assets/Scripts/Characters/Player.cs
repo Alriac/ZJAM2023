78c243632fe6fca6168de71fb1b60fa94311437b
d21927790d <dedepankeke@gmail.com>
2023-05-06 20:25:28 +0200

    Animaciones

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -6,21 +6,39 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float speed = 10.0f;
-
     Animator a;
+    SpriteRenderer sr;
 
     // Start is called before the first frame update
     void Start()
     {
-        a = GetComponent<Animator>();
+        a  = GetComponent<Animator>();
+        sr = GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0) * speed * Time.deltaTime;
-        if (Input.GetKeyDown(KeyCode.Space) && GameEvents.Ins.OnPlayerActionKey != null)
+        float horizontal_move = Input.GetAxis("Horizontal");
+        float vertical_move   = Input.GetAxis("Vertical");
+        if (horizontal_move == 0) {
+            a.SetBool("HorWalk", false);
+        } else {
+            a.SetBool("HorWalk", true);
+            if (horizontal_move > 0) {
+                if (!sr.flipX) {
+                    sr.flipX = true;
+                }
+            } else if (horizontal_move < 0) {
+                if (sr.flipX) {
+                    sr.flipX = false;
+                }
+            }
+        }
+        transform.position += new Vector3(horizontal_move, vertical_move, 0) * speed * Time.deltaTime;
+        if (Input.GetKeyDown(KeyCode.Space) && GameEvents.Ins.OnPlayerActionKey != null) {
             GameEvents.Ins.OnPlayerActionKey();
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
