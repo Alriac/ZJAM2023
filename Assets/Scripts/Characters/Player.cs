010997babe73ff31c7eebb60340ddf869c646282
d21927790d <dedepankeke@gmail.com>
2023-05-07 07:49:11 +0200

    Mas animaciones :(

--- Assets/Scripts/Characters/Player.cs
+++ Assets/Scripts/Characters/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 [DisallowMultipleComponent]
 public class Player : MonoBehaviour
 {
-    public float speed = 10.0f;
+    public float speed = 5.0f;
     Animator a;
     SpriteRenderer sr;
     Rigidbody2D rb;
