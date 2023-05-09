4d6ec6af73e9c1a57a9b5d38748d2b280c3bb7f0
Lariac <larariag@gmail.com>
2023-05-09 21:48:29 +0200

    Añadido sonido de toctoctoc y sincronizado con animación.

--- Assets/Scripts/Characters/MoneyDesperatePerson.cs
+++ Assets/Scripts/Characters/MoneyDesperatePerson.cs
@@ -1,13 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
+
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 [DisallowMultipleComponent]
 public class MoneyDesperatePerson : MonoBehaviour
 {
     public float AngrynessLevel = 0; // Max is always 100
     public AngrynessForEnergy[] AngrynessPerObject;
+    public ToctocSounds[] ToctocSoundsPerAngryness;
 
+    AudioSource asource;
+    float toctocLastTime;
+    float toctocDelay;
+    Animator anim;
 
     [System.Serializable]
     public struct AngrynessForEnergy
@@ -17,8 +24,22 @@ public class MoneyDesperatePerson : MonoBehaviour
         public float AngrynessGeneratedPerSecond;
         // Idea: Multiplicador por tiempo transcurrido.
     }
+    [System.Serializable]
+    public struct ToctocSounds
+    {
+        public AudioClip Clip;
+        public float FromAngrynessLevel;
+        public float MinFrequency;
+        public float MaxFrequency;
+        public int KnockAmount;
+        // Idea: Multiplicador por tiempo transcurrido.
+    }
 
-    // Start is called before the first frame update
+    private void Awake()
+    {
+        anim = GetComponent<Animator>();
+        asource = GetComponent<AudioSource>();
+    }
     void Start()
     {
         GameEvents.Ins.OnObjectSwitched += OnObjectSwitched;
@@ -49,5 +70,23 @@ public class MoneyDesperatePerson : MonoBehaviour
         }
         if (GameEvents.Ins.OnScoreChanged != null)
             GameEvents.Ins.OnScoreChanged(EnumScoreType.MoneyDesperatePersonAngryness, this.AngrynessLevel);
+
+
+        if (toctocLastTime + toctocDelay < Time.time)
+        {
+            // Comprueba si ha de trucar.
+            for (int i = ToctocSoundsPerAngryness.Length - 1; i >= 0; i--)
+            {
+                if (AngrynessLevel >= ToctocSoundsPerAngryness[i].FromAngrynessLevel)
+                {
+                    toctocDelay = Random.Range(ToctocSoundsPerAngryness[i].MinFrequency, ToctocSoundsPerAngryness[i].MaxFrequency);
+                    toctocLastTime = Time.time;
+                    anim.SetTrigger("knocks" + ToctocSoundsPerAngryness[i].KnockAmount);
+                    asource.PlayOneShot(ToctocSoundsPerAngryness[i].Clip);
+                    break;
+                }
+                toctocDelay = 1f; // Si no truca, espera 1 animación para comprobar de nuevo.
+            }
+        }
     }
 }
